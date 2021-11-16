30b12eb0be23d1c7b444ec9f02340c2997179619
machida-being <machida@beincorp.co.jp>
2021-11-16 18:53:30 +0900

    乱数でtitleとcontentを管理

--- main.rb
+++ main.rb
@@ -3,24 +3,23 @@ require 'sinatra/reloader'
 require 'json'
 
 
-get '/top' do
+get '/memos' do
+    @memos = File.open("json/test.json") {|f| JSON.load(f)}
     erb:top
 end
 
-post '/top' do
-    @title = params['title']
-    @content = params['content']
-    File.open("json/test.json", 'w') do |file|
-        json = {"id" => 1, "title" => params['title'], "content" => params['content']}
-        str = JSON.dump(json, file)
-    end
+post '/memos' do
+    @memos = File.open("json/test.json") {|f| JSON.load(f)}
+    @memos[SecureRandom.uuid] = {"title" => params['title'], "content" => params['content']}
+    File.open("json/test.json", 'w') {|f| JSON.dump(@memos, f)}
     erb:top
 end
 
-get '/top/:title' do
-    File.open("json/test.json", 'r') do |file|
-        @hash = JSON.load(file)
-    end
+get '/memos/:id/details' do
+    memos = File.open("json/test.json") {|f| JSON.load(f)} 
+    @id = params[:id]
+    @memo = memos[@id]
+    
     erb:show
 end
 
@@ -28,7 +27,18 @@ get '/new' do
     erb:new
 end
 
-post '/new' do 
-    @title = params['title']
-    erb:new
+get 'memos/:id/edit' do
+    memos = File.open("json/test.json") {|f| JSON.load(f)} 
+    @id = params[:id]
+    @memo = memos[@id]
+    erb:edit
 end
+
+patch 'memos/:id/edit' do
+    id = params[:id]
+    @memos = File.open("json/test.json") {|f| JSON.load(f)}
+    @memos[id] = {"title" => params['title'], "content" => params['content']}
+    File.open("json/test.json", 'w') {|f| JSON.dump(@memos, f)}
+    erb:edit
+end
+
