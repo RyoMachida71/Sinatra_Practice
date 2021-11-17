383a28028a26d0079af345eadca408494f47f66a
machida-being <machida@beincorp.co.jp>
2021-11-17 15:44:35 +0900

    XSS対策と、jsonファイルが存在しない場合に"{}"を加えてファイルを新規作成する処理を追加

--- main.rb
+++ main.rb
@@ -3,8 +3,16 @@ require 'sinatra/reloader'
 require 'json'
 
 
+helpers do
+    def h(text)
+        Rack::Utils.escape_html(text)
+    end
+end
+
+
 get '/memos' do
-    @memos = File.open("json/test.json") {|f| JSON.load(f)}
+    File.exist?("json/test.json") ? (@memos = File.open("json/test.json") {|f| JSON.load(f)}) : (File.open("json/test.json", 'w')  {|f| f.write("{}")}
+    @memos = File.open("json/test.json") {|f| JSON.load(f)})
     erb:top
 end
 
@@ -12,7 +20,7 @@ post '/memos' do
     @memos = File.open("json/test.json") {|f| JSON.load(f)}
     @memos[SecureRandom.uuid] = {"title" => params['title'], "content" => params['content']}
     File.open("json/test.json", 'w') {|f| JSON.dump(@memos, f)}
-    erb:top
+    redirect to "/memos"
 end
 
 get '/memos/:id/details' do
