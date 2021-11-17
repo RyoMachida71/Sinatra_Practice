33b34ac9e70730fe22ef33f9399acd0276066843
machida-being <machida@beincorp.co.jp>
2021-11-17 11:11:01 +0900

    メモ一覧の表示、新規メモの追加、既存メモの編集、削除の機能を実装した

--- main.rb
+++ main.rb
@@ -19,7 +19,6 @@ get '/memos/:id/details' do
     memos = File.open("json/test.json") {|f| JSON.load(f)} 
     @id = params[:id]
     @memo = memos[@id]
-    
     erb:show
 end
 
@@ -27,18 +26,24 @@ get '/new' do
     erb:new
 end
 
-get 'memos/:id/edit' do
+get '/memos/:id/edit' do
     memos = File.open("json/test.json") {|f| JSON.load(f)} 
     @id = params[:id]
     @memo = memos[@id]
     erb:edit
 end
 
-patch 'memos/:id/edit' do
+patch '/memos/:id' do
     id = params[:id]
     @memos = File.open("json/test.json") {|f| JSON.load(f)}
     @memos[id] = {"title" => params['title'], "content" => params['content']}
     File.open("json/test.json", 'w') {|f| JSON.dump(@memos, f)}
-    erb:edit
+    redirect to '/memos'
 end
 
+delete '/memos/:id' do
+    memos = File.open("json/test.json") {|f| JSON.load(f)}
+    memos.delete(params[:id])
+    File.open("json/test.json", 'w') {|f| JSON.dump(memos, f)} 
+    redirect to '/memos'
+end
